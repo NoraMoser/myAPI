7ecf8f512a4b31cf0cd35f9bbf71557536a322fc
Nora Moser <nora.moser@palinode.io>
2025-03-11 11:55:24 -0700

    so far

--- obj/Debug/net9.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a838a6bb822059915f6266d6a985348007ce39e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ceb972c77112c8615b263f2f076a325335ec842")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
